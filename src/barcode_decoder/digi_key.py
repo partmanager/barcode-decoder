b50f36fa4ce65256d435508d16b71fb5ff4c437b
sakoPO <sako.po@gmai.com>
2024-06-01 21:59:32 +0200

    Fixed digikey decoder.

--- src/barcode_decoder/digi_key.py
+++ src/barcode_decoder/digi_key.py
@@ -10,31 +10,37 @@ def decode_digi_key_barcode(code_type: CodeType, barcode):
 def decode_datamatrix(barcode):
     data = barcode.split(']')
 
-    result = Result(distributor="DigiKey")
-    for param in data:
-        if param.startswith('P'):
-            result.don = param.removeprefix('P')
-        elif param.startswith('1P'):
-            result.mon = param.removeprefix('1P')
-        elif param.startswith('K'):
-            result.order_number = {"number": param.removeprefix('K')}
-        elif param.startswith('Q'):
-            result.quantity = float(param.removeprefix('Q'))
-        elif param.startswith('9D') and len(param) > 2:
-            result.date_code = param.removeprefix('9D')
-        elif param.startswith('1T') and len(param) > 2:
-            result.LOT = param.removeprefix('1T')
-        elif param.startswith('4L'):
-            result.coo = param.removeprefix('4L')
-        elif param.startswith('4K'):
-            result.order_number["position"] = param.removeprefix('4K')
-        elif param.startswith('10K'):
-            pass
-        elif param.startswith('11K'):
-            pass
-        elif param.startswith('12Z'):
-            pass  # part id
-        elif param.startswith('13Z'):
-            pass  # load id
+    if data[0] == "[)>^06":
+        result = Result(distributor="DigiKey")
+        digikey_code = False
+        for param in data:
+            if param.startswith('P'):
+                result.don = param.removeprefix('P')
+            elif param.startswith('1P'):
+                result.mon = param.removeprefix('1P')
+            elif param.startswith('K'):
+                result.order_number = {"number": param.removeprefix('K')}
+            elif param.startswith('Q'):
+                result.quantity = float(param.removeprefix('Q'))
+            elif param.startswith('9D') and len(param) > 2:
+                result.date_code = param.removeprefix('9D')
+            elif param.startswith('1T') and len(param) > 2:
+                result.LOT = param.removeprefix('1T')
+            elif param.startswith('4L'):
+                result.coo = param.removeprefix('4L')
+            elif param.startswith('4K'):
+                result.order_number["position"] = param.removeprefix('4K')
+            elif param.startswith('10K'):
+                pass
+            elif param.startswith('11K'):
+                pass
+            elif param.startswith('12Z'):
+                pass  # part id
+            elif param.startswith('13Z'):
+                pass  # load id
+            elif param.startswith('20Z'):
+                zeros = param.removeprefix('20Z')
+                if zeros.count('0') == len(zeros) and len(zeros) > 80:
+                    digikey_code = True
 
-    return result if result.is_valid() else None
+        return result if result.is_valid() and digikey_code else None
