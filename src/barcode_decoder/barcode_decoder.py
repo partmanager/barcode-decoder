59918fd3a874ce14c07faec33edabef65da444bc
sakoPO <sako.po@gmai.com>
2024-06-01 14:36:32 +0200

    Added console application file.

--- src/barcode_decoder/barcode_decoder.py
+++ src/barcode_decoder/barcode_decoder.py
@@ -5,38 +5,19 @@ from .tme import decode_tme_barcode
 from .wurth_elektronik import decode_wurth_elektronik
 from .scanner import detect_code_type
 
+_decoders = [decode_digi_key_barcode,
+             decode_farnell_barcode,
+             decode_mouser_barcode,
+             decode_tme_barcode,
+             decode_wurth_elektronik]
 
-decoders = [decode_digi_key_barcode,
-            decode_farnell_barcode,
-            decode_mouser_barcode,
-            decode_tme_barcode,
-            decode_wurth_elektronik]
 
-
-def decode_barcode(barcode: str):
+def decode(barcode: str):
     """Takes a scanned barcode and returns a decoded parameters in form of a list of dictionaries"""
     result = []
     barcode = barcode.upper().rstrip()
     code_type, code = detect_code_type(barcode)
-    for decoder in decoders:
+    for decoder in _decoders:
         decoded = decoder(code_type, code)
         result.append(decoded)
     return result
-
-
-def pretty_print(decoded_barcode):
-    print('==============================')
-    for result in decoded_barcode:
-        if result:
-            print('----')
-            print(result)
-
-
-if __name__ == '__main__':
-    print("Barcode Decoder, press Enter to exit")
-    while True:
-        barcode = input("Barcode? ")
-        if len(barcode) <= 1:
-            break
-        results = decode_barcode(barcode)
-        pretty_print(results)
